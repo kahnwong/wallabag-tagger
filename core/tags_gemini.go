80276c3794fc582b4d76bd2b1d3421207f6eb696
Karn Wong <karn@karnwong.me>
2025-03-16 13:05:46 +0700

    refactor(tags): use template for prompt

--- core/tags_gemini.go
+++ core/tags_gemini.go
@@ -1,8 +1,11 @@
 package core
 
 import (
+	"bytes"
 	"context"
+	"embed"
 	"fmt"
+	"text/template"
 
 	"github.com/google/generative-ai-go/genai"
 	"github.com/microcosm-cc/bluemonday"
@@ -10,6 +13,26 @@ import (
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
+
+//go:embed resources/*
+var templatesFS embed.FS
+
+func renderPrompt(templatePath string, data any) string {
+	// init template
+	tmpl, err := template.ParseFS(templatesFS, templatePath)
+	if err != nil {
+		log.Fatal().Msgf("Error parsing template: %s", templatePath)
+	}
+
+	// render template
+	var tpl bytes.Buffer
+	err = tmpl.Execute(&tpl, data)
+	if err != nil {
+		log.Fatal().Msgf("Error rendering now playing: %s", templatePath)
+	}
+
+	return tpl.String()
+}
 
 func GeminiGetTags(content string) (string, error) {
 	var err error
@@ -30,7 +53,9 @@ func GeminiGetTags(content string) (string, error) {
 	contentSanitized := p.Sanitize(
 		content,
 	)
-	prompt := fmt.Sprintf("You are a bot in a read-it-later app and your responsibility is to help with automatic tagging.\nPlease analyze the text between the sentences \"CONTENT START HERE\" and \"CONTENT END HERE\" and suggest relevant tags that describe its key themes, topics, and main ideas. The rules are:\n- The available tag options should be: data, devops, tools, software engineering, misc.\n- The content can include text for cookie consent and privacy policy, ignore those while tagging.\n- Only single tag should be applied.\n- If there are no good tags, leave the array empty.\n- You must respond in JSON using this JSON schema:      Tags = {\"tags\": list[str]}      Return: Tags\nCONTENT START HERE\n\n<%s>\n\nCONTENT END HERE\nYou must respond in JSON with the key \"tags\" and the value is an array of string tags.", contentSanitized)
+	prompt := renderPrompt("resources/prompt.txt", map[string]interface{}{
+		"Content": contentSanitized,
+	})
 	iter := model.GenerateContentStream(ctx, genai.Text(prompt))
 
 	var output string
